88cc72926840eb18b30b68fc0e72f9d4a9a505de
[dyad] <seldo6@gmail.com>
2025-12-17 00:24:01 -0500

    Updated index.tsx

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './srs/index.css' // Make sure this line exists!
+import './src/index.css' // Make sure this line exists!
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
